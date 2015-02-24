76e0606b72f4e78d2b53616622eb4cee9f644a3a
Stefan Haertel <haertel.dev@gmail.com>
2015-02-24 22:02:40 +0100

    create simple window

--- src/de/eightbitboy/memory/Memory.java
+++ src/de/eightbitboy/memory/Memory.java
@@ -1,7 +1,20 @@
 package de.eightbitboy.memory;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class Memory {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello Stefanie!");
+
+		JFrame frame = new JFrame("Memory");
+		frame.setLocation(400, 400);
+		frame.setSize(400, 400);
+
+		Color frameBackground = new Color(231, 235, 202);
+
+		frame.getContentPane().setBackground(frameBackground);
+		frame.setVisible(true);
 	}
 }
